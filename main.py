92a07c93a25e8973727d16f23b3ffe19b580c11e
asyrofist <rakhasyrofist@gmail.com>
2019-10-19 11:32:32 +0700

    perubahan

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 print ("halo")
+
+# git config --global user.email "email_anda@home.com"
+# git config --global user.name "user_anda"
+# git add.
+# git commit -m "tambah_file"
+# git push -u origin master
