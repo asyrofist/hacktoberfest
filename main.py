0e499acc06f1a4f1b0a5ebb48c55a178f558766b
asyrofist <rakhasyrofist@gmail.com>
2019-10-19 13:00:50 +0700

    perubahan pada file

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ print ("halo")
 
 # git config --global user.email "email_anda@home.com"
 # git config --global user.name "user_anda"
-# git add.
-# git commit -m "tambah_file"
-# git push -u origin master
+# git add. // file yang ditambahkan
+# git commit -m "tambah_file" // komentar commit
+# git push -u origin master //push
 
 # git status
 # git add nama_file.py
 # git commit -m "tambah_file"
 # git push -u origin master
+
+# git clone // project baru
+# git pull // update
